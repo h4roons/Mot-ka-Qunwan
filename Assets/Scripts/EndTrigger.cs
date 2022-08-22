9c29ec0e82053e51b1fff4dfe407c12f0c36b950
DESKTOP-12QMKN9\HP <haroonsohail323@gmail.com>
2022-08-22 16:46:39 +0500

    End Trigger script fixed

--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class endTrigger : MonoBehaviour
+public class EndTrigger : MonoBehaviour
 {
     public Gmanager gm;
 
