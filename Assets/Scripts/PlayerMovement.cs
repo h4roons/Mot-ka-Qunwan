9babbec59648599b13b9797f859d188d25e5a5a5
DESKTOP-12QMKN9\HP <haroonsohail323@gmail.com>
2022-08-22 17:31:28 +0500

    Adding up some BG Music to increase the game feel

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float upforce = 10f;
     [SerializeField] private float downforce;
-   
+    [SerializeField] AudioClip BikeEngine;
+    AudioSource audioSource;
+    private void Start()
+    {
+        audioSource= GetComponent<AudioSource>();
+    }
+    
 
     void FixedUpdate()
     {
@@ -18,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
                 Touch touch = Input.GetTouch(0);
                 if (touch.phase == TouchPhase.Stationary)
                 {
+                    audioSource.PlayOneShot(BikeEngine);
                     rb.AddForce(0, 0, upforce * Time.deltaTime, ForceMode.Force);
 
                 }
@@ -25,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
             }
 
             rb.AddForce(0, 0, downforce * Time.deltaTime, ForceMode.Acceleration);
+            audioSource.Stop();
 
             if (rb.position.y < -1f)
             {
