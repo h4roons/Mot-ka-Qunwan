25f073182881b3b94310ccb8e366163986a6b73b
DESKTOP-12QMKN9\HP <haroonsohail323@gmail.com>
2022-09-04 02:46:57 +0500

    NPCs Injected

--- Assets/Scripts/obstacletrigg2.cs
+++ Assets/Scripts/obstacletrigg2.cs
@@ -9,6 +9,6 @@ public class obstacletrigg2 : MonoBehaviour
     {
         
         obstacle.transform.Rotate(0,90,90);
-        obstacle.GetComponent<CapsuleCollider>().enabled = true;
+        obstacle.GetComponent<BoxCollider>().enabled = true;
     }
 }
