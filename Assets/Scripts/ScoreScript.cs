6f92c40bb015e8b82f47920a019194fcdf97ef80
DESKTOP-12QMKN9\HP <haroonsohail323@gmail.com>
2022-09-10 01:30:48 +0500

    Added up a score script and prefabbed the money stash, you just need to put moneystash into the scene. Which can be found in Assets > Scenes > Prefabs

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScoreScript : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    private int ScoreNum;
+    public GameObject ParticleEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreNum = 0;
+        text.text = "Score : " + ScoreNum; 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Score")
+        {
+            Instantiate(ParticleEffect, transform.position, transform.rotation);
+            ScoreNum++;
+            Destroy(other.gameObject);
+            text.text = "Score : " + ScoreNum;
+        }
+    }
+}
