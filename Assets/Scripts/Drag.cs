8b9409c97cfdc360988f900dbbb80236552fa22b
DESKTOP-12QMKN9\HP <haroonsohail323@gmail.com>
2022-09-11 05:07:47 +0500

    Bug fixes and animated the bike.!

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -8,10 +8,17 @@ public class Drag : MonoBehaviour
     private Touch touch;
     [SerializeField]private float speedModifier;
     public GameObject pickupEffect;
-   // public Transform gameObj;
+    // public Transform gameObj;
     //public Transform handlebar;
-   
-        // Start is called before the first frame update
+    public Transform bike_Child;
+    public Vector3 idle=Vector3.zero;
+    public Vector3 tilt;
+    public float rotation_speed;
+    public Vector3 targetRotation;
+    float currentPOS = 0f;
+    float previousPOS= -6.12f;
+
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -28,20 +35,49 @@ public class Drag : MonoBehaviour
         {
 
             touch = Input.GetTouch(0);
-            
+           
             if (touch.phase == TouchPhase.Moved)
             {
                 //gameObj.transform.eulerAngles = new Vector3(
-            //gameObj.transform.eulerAngles.x,
-           // gameObj.transform.eulerAngles.y + 180,
-          // gameObj.transform.eulerAngles.z);
-                                           
+                //gameObj.transform.eulerAngles.x,
+                // gameObj.transform.eulerAngles.y + 180,
+                // gameObj.transform.eulerAngles.z);
 
+                
                 // handlebar.transform.Rotate(new Vector3(0,Mathf.Clamp(transform.rotation.y+touch.deltaPosition.y*speedModifier,-67.3f,-13.3f),0));
                 transform.position = new Vector3(
                     transform.position.x,
                     transform.position.y,
                     Mathf.Clamp(transform.position.z + touch.deltaPosition.y * speedModifier, -11.17f, -0.51f));
+
+
+                currentPOS = transform.position.z;
+                if (currentPOS < previousPOS)
+                {
+                    bike_Child.transform.localRotation = Quaternion.Slerp(bike_Child.transform.localRotation, Quaternion.Euler(targetRotation), rotation_speed * Time.deltaTime);
+                }
+                if (currentPOS > previousPOS)
+                {
+                    bike_Child.transform.localRotation = Quaternion.Slerp(bike_Child.transform.localRotation, Quaternion.Euler(-targetRotation), rotation_speed * Time.deltaTime);
+                }
+                if (currentPOS == previousPOS)
+                {
+                    bike_Child.transform.localRotation = Quaternion.Slerp(bike_Child.transform.localRotation, Quaternion.Euler(idle), rotation_speed * Time.deltaTime);
+                }
+                previousPOS = currentPOS;
+                
+                //if (transform.position.z < -6.12f)
+                //{
+                //    bike_Child.transform.localRotation = Quaternion.Slerp(bike_Child.transform.localRotation, Quaternion.Euler(targetRotation), rotation_speed * Time.deltaTime);
+                //}
+                //else if (transform.position.z > -6.12f)
+                //{
+                //    bike_Child.transform.localRotation = Quaternion.Slerp(bike_Child.transform.localRotation, Quaternion.Euler(-targetRotation), rotation_speed * Time.deltaTime);
+                //}
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                bike_Child.transform.localRotation = Quaternion.Slerp(bike_Child.transform.localRotation, Quaternion.Euler(idle), rotation_speed * Time.deltaTime);
             }
         }
         
